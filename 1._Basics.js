c489d64df9a8bc84d4b33505c962ca1c94353217
JullerMus <juliusclykkegaard@gmail.com>
2025-09-02 21:02:18 +0200

    Finished exercises

--- 1._Basics.js
+++ 1._Basics.js
@@ -9,6 +9,7 @@ const lastName = "Latif";
 // EXERCISE
 // show in the console
 // My first name is Anders and my last name is Latif
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 
 // --------------------------------------
@@ -20,6 +21,8 @@ const increment = 1;
 // Add the year plus the increment
 // The result should be 2025
 // You cannot touch the first or the second line
-
+console.log(Number(year) + increment);
+console.log(parseInt(year) + increment);
+console.log(+year + increment);
 
 // --------------------------------------
